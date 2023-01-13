34fb9158ad55960b9dbd9342590a29d1c2f31b63
nidou-bt <bentaaritn@gmail.com>
2023-01-13 11:34:13 +0100

    add input type

--- server/resolves.js
+++ server/resolves.js
@@ -7,6 +7,10 @@ export const resolvers = {
     company: (_root, { id }) => Company.findById(id),
   },
 
+  Mutation: {
+    createJob: (_root, { input }) => Job.create(input)
+  },
+
   Job: {
     company: ({ companyId }) => {
       return Company.findById(companyId);
